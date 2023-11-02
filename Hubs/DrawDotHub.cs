6ed61459d79fddff6c0b904c8a97337848b79a06
Nu <rileytrinh1411@gmail.com>
2023-11-01 20:56:49 -0700

    1st

---
+++ Hubs/DrawDotHub.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+public class DrawDotHub: Hub {
+   public async Task UpdateCanvas(int x, int y, string color) {
+      await Clients.All.SendAsync("updateDot",x, y, color);
+   }
+
+   public async Task ClearCanvas() {
+      await Clients.All.SendAsync("clearCanvas");
+   }
+}
